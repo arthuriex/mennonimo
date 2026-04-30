15d4f6d2f0b138b43461c0b29b6d5faea420400d
Mennonimo <art2535452523@gmail.com>
2026-04-29 22:26:17 -0300

    Modificações Iniciais na header

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,14 @@ export default function Home() {
   const ferramentas = ["Nmap", "Metasploit", "Wireshark"];
 
   return (
-    <main className="p-10 bg-slate-900 text-white min-h-screen">
+    <main className="bg-slate-900 text-white min-h-screen">
+      <header className="w-screen flex flex-row justify-center p-7 bg-black">
+        <h1 className="text-4xl font-bold">Mennonimo</h1>
+      </header>
       <h1 className="text-3xl font-bold border-b border-blue-500 pb-2 justify-self-center">
         Meu Arsenal de Segurança
       </h1>
       
-      <ul className="mt-5 space-y-2">
-        {ferramentas.map((item) => (
-          <li key={item} className="bg-slate-800 p-3 rounded shadow-sm hover:bg-slate-700 transition">
-            {item}
-          </li>
-        ))}
-      </ul>
     </main>
   );
 }
